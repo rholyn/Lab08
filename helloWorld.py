862eb0c6090b6581a2e85bc23ec807a43c713c00
rholyn <rholyn_dayrit@uri.edu>
2022-03-21 19:12:36 -0400

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,6 @@
-userName = input ("Hello! Please enter your name!")
-print ("Hello", userName, "nice to meet you")
+userName = input ("Hello! Please enter your facebook username!")
+userPassword = input ("Hello! Please enter your facebook password!")
+if (userName.lower() == "rholyn" and userPassword == "Dayrit"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
